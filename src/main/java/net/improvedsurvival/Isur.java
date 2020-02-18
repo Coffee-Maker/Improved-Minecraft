42bb628f11e877f9f8a5dda5bebdfb3be9aee206
Isor <isor.graphics@gmail.com>
2020-02-18 14:35:03 +0000

    Added better status effect registry

--- src/main/java/net/improvedsurvival/Isur.java
+++ src/main/java/net/improvedsurvival/Isur.java
@@ -11,15 +11,9 @@ import net.improvedsurvival.containers.GlazerContainer;
 import net.improvedsurvival.mixin_data.HoeExtensions;
 import net.improvedsurvival.registry.IsurBlocks;
 import net.improvedsurvival.registry.IsurFood;
-import net.improvedsurvival.status_effects.Chilling;
-import net.improvedsurvival.status_effects.CustomStatusEffect;
 import net.improvedsurvival.util.RandomBlockMapperStructureProcessor;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectType;
-import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.structure.processor.StructureProcessorType;
@@ -41,12 +35,6 @@ public class Isur implements ModInitializer {
     public static final ItemGroup ISUR = FabricItemGroupBuilder.build(new Identifier("isur", "improved_survival"), () -> new ItemStack(Blocks.FARMLAND));
 
     public static final Identifier GLAZER_CRAFTING = new Identifier(Isur.MODID, "glazer_crafting");
-
-    public static final StatusEffect COLD;
-    public static final StatusEffect FROST_BITE;
-    public static final StatusEffect SWEATING;
-    public static final StatusEffect HEATSTROKE;
-    public static final StatusEffect CHILLING;
 
     public static StructureProcessorType RANDOM_BLOCK_MAPPER;
 
@@ -74,20 +62,4 @@ public class Isur implements ModInitializer {
 
     static BlockState FROST_BERRY_BUSH_STATE = (BlockState)IsurBlocks.FROST_BERRY_BUSH.getDefaultState().with(FrostBerryBush.AGE, 3);
     static RandomPatchFeatureConfig FROST_BERRY_BUSH_CONFIG = (new RandomPatchFeatureConfig.Builder(new SimpleStateProvider(FROST_BERRY_BUSH_STATE), new SimpleBlockPlacer())).cannotProject().tries(64).whitelist(ImmutableSet.of(Blocks.GRASS_BLOCK)).build();
-
-    private static StatusEffect registerStatusEffect(int rawId, String id, StatusEffect entry) {
-        return (StatusEffect)Registry.register(Registry.STATUS_EFFECT, new Identifier("isur", id), entry);
-    }
-
-    public static PlayerInventory playerInv(){
-        return MinecraftClient.getInstance().player.inventory;
-    }
-
-    static{
-        COLD = registerStatusEffect(1507, "cold", (new CustomStatusEffect(StatusEffectType.HARMFUL, 12572927)));
-        FROST_BITE = registerStatusEffect(1507, "frost_bite", (new CustomStatusEffect(StatusEffectType.HARMFUL, 12572927)));
-        SWEATING = registerStatusEffect(1507, "sweating", (new CustomStatusEffect(StatusEffectType.HARMFUL, 12572927)));
-        HEATSTROKE = registerStatusEffect(1507, "heatstroke", (new CustomStatusEffect(StatusEffectType.HARMFUL, 12572927)));
-        CHILLING = registerStatusEffect(1507, "chilling", (new Chilling(StatusEffectType.NEUTRAL, 12572927)));
-    }
 }
--- src/main/java/net/improvedsurvival/IsurClient.java
+++ src/main/java/net/improvedsurvival/IsurClient.java
@@ -1,26 +1,27 @@
 package net.improvedsurvival;
 
-import net.improvedsurvival.containers.GlazerContainer;
-import net.improvedsurvival.containers.GlazerScreen;
-import net.improvedsurvival.registry.IsurBlocks;
-import net.improvedsurvival.rendering.PlayerOverlayIndicators;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
 import net.fabricmc.fabric.api.event.client.ItemTooltipCallback;
+import net.improvedsurvival.containers.GlazerContainer;
+import net.improvedsurvival.containers.GlazerScreen;
+import net.improvedsurvival.registry.IsurBlocks;
+import net.improvedsurvival.rendering.PlayerOverlayIndicators;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 
-
 public class IsurClient implements ClientModInitializer
 {
     @Override
     public void onInitializeClient() {
-        ScreenProviderRegistry.INSTANCE.<GlazerContainer>registerFactory(Isur.GLAZER_CRAFTING, container -> new GlazerScreen(container, Isur.playerInv(), new TranslatableText("container.glazer", new Object[0])));
+        ScreenProviderRegistry.INSTANCE.<GlazerContainer>registerFactory(Isur.GLAZER_CRAFTING, container -> new GlazerScreen(container, playerInv(), new TranslatableText("container.glazer", new Object[0])));
 
         HudRenderCallback.EVENT.register(t -> PlayerOverlayIndicators.renderPumpkinOverlay());
         BlockRenderLayerMap.INSTANCE.putBlock(IsurBlocks.FROST_BERRY_BUSH, RenderLayer.getCutout());
@@ -41,5 +42,10 @@ public class IsurClient implements ClientModInitializer
                 lines.add(2, new LiteralText(Formatting.GRAY + "" + Formatting.ITALIC + "None"));
             }
         });
+    }
+
+    public static PlayerInventory playerInv(){
+        MinecraftClient client = MinecraftClient.getInstance();
+        return client.player.inventory;
     }
 }
